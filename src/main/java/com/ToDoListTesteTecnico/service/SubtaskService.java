5ca3e66b17ce0fbf43bfa7f99e09070a3d9b67b1
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2025-08-12 19:16:50 -0300

    feati11:Implementation of subtask funcionality

--- src/main/java/com/ToDoListTesteTecnico/service/SubtaskService.java
+++ src/main/java/com/ToDoListTesteTecnico/service/SubtaskService.java
@@ -29,10 +29,27 @@ public class SubtaskService implements SubTaskServiceContract {
     @Override
     public TaskVO addSubTask(SubTaskRequest subTaskRequest) {
 
+        if(subTaskRequest.getTaskId().equals(subTaskRequest.getSubTaskId())) {
+
+            throw new RuntimeException("the two ids are equals, please verify and try again");
+        }
 
         TaskVO taskById = taskService.getTaskById(subTaskRequest.getTaskId());
         TaskVO subtaskById = taskService.getTaskById(subTaskRequest.getSubTaskId());
+
+        verifyIfHaveDuplicatedSubtask(subTaskRequest.getSubTaskId(),taskById);
+
+        if(subtaskById.getSubTasks().size() > 0) {
+
+            throw new RuntimeException("Is not posible add tasks to subtasks!!");
+        }
+
+
         TaskEntity subTaskEntity = BuilderMapper.parseObject(new TaskEntity(), subtaskById);
+
+        for(TaskEntity taskEntity : taskById.getSubTasks()) {
+
+        }
         ValidatorUtils.checkFieldNotNullAndNotEmptyOrThrowException(subTaskEntity, TASK_NOT_FOUND_EXCEPTION_MESSAGE, FieldNotFoundException.class);
         taskById.getSubTasks().add(subTaskEntity);
         TaskEntity taskEntity = BuilderMapper.parseObject(new TaskEntity(), taskById);
@@ -44,5 +61,15 @@ public class SubtaskService implements SubTaskServiceContract {
     @Override
     public TaskVO updateSubTask(SubTaskUpdateRequest subTaskUpdateRequest) {
         return null;
+    }
+
+    private void verifyIfHaveDuplicatedSubtask(String subTaskId, TaskVO taskVO) {
+
+        for (int i = 0; i < taskVO.getSubTasks().size(); i++) {
+
+            if(subTaskId.equals(taskVO.getSubTasks().get(i).getId()))
+                throw new RuntimeException("This subtask is already associated with this task!");
+
+        }
     }
 }
